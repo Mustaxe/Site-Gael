69288814a2544b82fe58de737cfb0da67748e436
rmgcoder <rmgcoder@gmail.com>
2016-06-23 10:43:48 -0300

    Ajuste path form contato

--- js/scripts.js
+++ js/scripts.js
@@ -131,7 +131,7 @@ jQuery(document).ready(function ($) {
 
 			jQuery.ajax({
 				type: "POST",
-				url: "http://homologacao.gael.ag/service/contatos",
+				url: "http://gael.ag/service/contatos",
 				data: dados,
 				success: function( data )
 				{
